a3db409335f3e21640499bd319c0bcc4c56e8bc1
MichaelVoiun <supermishka3@gmail.com>
2022-09-07 22:57:57 +0300

    Первый коммит

--- src/Book.java
+++ src/Book.java
@@ -3,4 +3,25 @@ public class Book {
     public Author author;
     public int publicationYear;
 
+    public Book(String name, Author author, int publicationYear) {
+        this.name = name;
+        this.author = author;
+        this.publicationYear = publicationYear;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
 }
